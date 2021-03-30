2ebb3108168cd685505d339296c6737f9365439b
alexandr-efremoff <alexandr.efremoff@gmail.com>
2021-03-30 23:12:36 +0300

    Complete parse full name task.

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -3,12 +3,27 @@ package ru.skillbranch.devintensive.utils
 object Utils {
 
     fun parseFullName(fullName: String?) : Pair<String?, String?> {
-        // TODO FIX
-        val parts: List<String>? = fullName?.split(" ")
-        val firstName = parts?.getOrNull(0)
-        val lastName = parts?.getOrNull(1)
 
-//        return Pair(firstName, lastName)
-        return firstName to lastName
+        var fullNamePair: Pair<String?, String?> = null to null
+
+        when (fullName) {
+            null, "", " "  -> {
+                fullNamePair = null to null
+            }
+            else -> {
+                val parts: List<String> = fullName.split(" ")
+
+                val firstName = parts.getOrNull(0)
+                val lastName = parts.getOrNull(1)
+
+                if ((firstName != null && firstName.isNotEmpty()) && (lastName != null && lastName.isNotEmpty())) {
+                    fullNamePair = firstName to lastName
+                } else if ((firstName != null && firstName.isNotEmpty()) && lastName == null) {
+                    fullNamePair = firstName to null
+                }
+            }
+        }
+
+        return fullNamePair
     }
 }
